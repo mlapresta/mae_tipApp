141d3c6f880cca7ce02370fb121122a4cef99ee5
Mae LaPresta <mae.lapresta@gmail.com>
2019-09-05 20:40:29 -0500

    Initial Commit

---
+++ mae_tipapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mae_tipapp
+//
+//  Created by Mae LaPresta on 9/5/19.
+//  Copyright © 2019 Mae LaPresta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
